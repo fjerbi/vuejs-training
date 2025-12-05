4ec1edb5ad42a98ddcad96a83c5877f6617a7f3b
Firas Jerbi <firasjerbiv2@gmail.com>
2025-12-05 16:54:23 +0100

    Home cards

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import Hero from "./components/Hero.vue";
+import HomeCards from "./components/HomeCards.vue";
 import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
   <Navbar />
   <Hero />
+
+  <HomeCards />
 </template>
