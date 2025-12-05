16058e9071974159344c15ab04bbe3e607d8b11c
Firas Jerbi <firasjerbiv2@gmail.com>
2025-12-05 17:13:50 +0100

    Job Listings

---
+++ src/components/JobListings.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { defineProps, ref } from "vue";
+import jobData from "../jobs2.json";
+import JobListing from "./JobListing.vue";
+
+const jobs = ref(jobData); //similar to useState in React
+
+defineProps({
+  limit: Number,
+  showButton: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <section class="bg-blue-50 px-4 py-10">
+    <div class="cpontainer-xl lg-container m-auto">
+      <h2 class="text-3xl text-green-500 font-bold mb-6 text-center">
+        Job Listings
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <JobListing
+          v-for="job in jobs.slice(0, limit || jobs.length)"
+          :key="job.id"
+          :job="job"
+        />
+      </div>
+    </div>
+  </section>
+  <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+    <a
+      :href="'/jobs'"
+      class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+      >View All Jobs</a
+    >
+  </section>
+</template>
