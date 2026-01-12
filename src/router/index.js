1cbb347a4944e499e2814ae350fae340e7de0110
Firas Jerbi <firasjerbiv2@gmail.com>
2026-01-12 11:24:09 +0100

    Not Found Page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
 import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +14,11 @@ const router = createRouter({
       path: "/jobs",
       name: "jobs",
       component: JobsView,
+    },
+    {
+      path: "/:catchAll(.*)*",
+      name: "not-found",
+      component: NotFoundView,
     },
   ],
 });
